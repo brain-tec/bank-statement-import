b5e336e2deddd06904f02234cf8a85ae8ab73efb
BT-nfrei <nicolas.frei@braintec-group.com>
2018-02-08 19:15:06 +0100

    [IMP] The name of the bank.statement.line should be the BVR reference number for the matching.

--- account_bank_statement_import_camt/models/parser.py
+++ account_bank_statement_import_camt/models/parser.py
@@ -59,7 +59,6 @@ class CamtParser(models.AbstractModel):
         self.add_value_from_node(
             ns, node, [
                 './ns:AddtlTxInf',
-                './ns:RltdPties/ns:Dbtr/ns:Nm',
                 './ns:RmtInf/ns:Strd/ns:CdtrRefInf/ns:Ref'
             ], transaction, 'name', join_str='\n')
         # eref
